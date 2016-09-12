1f41f954d919d5ef02fefc28e8435dee97f47b94
michellehieuhau <michellehau@hotmail.com>
2016-09-11 21:37:36 -0400

    Add files via upload

---
+++ improvedprogram10pg181.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+ifstream inData;
+ofstream outData;
+const double PI = acos(-1.0);
+float radius;
+float circumference;
+float area;
+string fileName;
+
+int main()
+{
+    cout << "Please input file name: ";
+    cin >> fileName;
+    ifstream inData(fileName.c_str());
+    ofstream outData("outData.dat");
+
+    if (!inData.is_open() || !outData.is_open())
+    {
+        cout << "File did not open properly.";
+        return 1;
+    }
+
+    inData >> radius;
+    circumference = radius * 2 * PI;
+    area = radius * radius * PI;
+
+    cout << "For the first circle, the circumference is: "
+         << circumference << " and the area is " << area << endl;
+
+    outData << radius << " " << circumference << " " << area
+            << endl;
+
+    inData.close();
+    outData.close();
+
+    return 0;
+}
