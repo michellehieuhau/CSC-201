f6330b96dfd851ba9f1528261d6628698d395871
michellehieuhau <michellehau@hotmail.com>
2016-10-23 14:46:27 -0400

    Update AddRecord.cpp

--- AddressBook/AddRecord.cpp
+++ AddressBook/AddRecord.cpp
@@ -24,40 +24,72 @@ void NormalizeRecord(string& userRecord)
 //---------------------------------------------------------------------------
 // Outputs: New record
 // Asks user for new record elements
-// done by Robert, takes user inputs for new information appended to the file and stores them into struct data type variables
+// Taking user inputs for new information appended to the file and stores them into struct data type variables
 void AddRecord(entryType& userRecord)
 {
     string userPhoneNumber;     //local variable
 
-    cout << "Please input some information for a new entry to the Address Book." << endl;       //user prompts
-    cout << "Please enter a name for the new entry: " << endl;
-    cout << "Input in the format of Title, First Name, Middle Initial, Last Name, ... please include all commas." << endl;
-    getline(cin, userRecord.name.firstName,',');       //stores user inputs into struct variables
-    getline(cin, userRecord.name.lastName,'\n');
+void AddRecord(entryType& userRecord)
+{
+	string phone;
+	do
+	{
+		cout << "Enter first name: ";
+		getline(cin, userRecord.name.firstName);
+	} while (!ValidName(userRecord.name.firstName));
+	NormalizeRecord(userRecord.name.firstName);
+	
+	do
+	{
+		cout << "Enter last name: ";
+		getline(cin, userRecord.name.lastName);
+	} while (!ValidName(userRecord.name.lastName));
+	NormalizeRecord(userRecord.name.lastName);
+	
+	do
+	{
+		cout << "Enter street address: ";
+		getline(cin, userRecord.address.street);
+		cout << "Enter city: ";
+		getline(cin, userRecord.address.city);
+		cout << "Enter state: ";
+		getline(cin, userRecord.address.state);
+		userRecord.address.state = NormalizeString(userRecord.address.state);
+		cout << "Enter zip code: ";
+		getline(cin, userRecord.address.zip);
+	} while (!ValidAddress(userRecord.address));
+	NormalizeRecord(userRecord.address.street);
+	NormalizeRecord(userRecord.address.city);
+	NormalizeString(userRecord.address.state);
+	
+	do
+	{
+		cout << "Enter phone number in the form (XXX) XXX-XXXX: ";
+		getline(cin, phone);
+	} while (!ValidPhone(phone));
+	
+	stringstream(phone.substr(phone.find('(')+1,phone.find(')'))) >> userRecord.phone.areaCode;
+	stringstream(phone.substr(phone.find(')')+1,phone.find('-'))) >> userRecord.phone.prefix;
+	stringstream(phone.substr(phone.find('-')+1))>> userRecord.phone.number;
+	
+	do
+	{
+		cout << "Enter email address: ";
+		cin >> userRecord.media.email;
+	} while (!ValidMedia(userRecord.media.email, 'E'));
+	
+	do
+	{
+		cout << "Enter facebook username: ";
+		cin >> userRecord.media.facebook;
+	} while (!ValidMedia(userRecord.media.facebook, 'F'));
 
-    cout << endl;
-    cout << "Please enter a phone number: " << endl;        //user prompts
-    cout << "Enter the phone number in the format of (XXX) XXX-XXXX " << endl;
-    getline(cin, userPhoneNumber,'\n');         //puts phone number all into one string
-    stringstream(userPhoneNumber.substr(userPhoneNumber.find('(')+1 , userPhoneNumber.find(')'))) >> userRecord.phone.areaCode;     //took the code from above in the GetPhone function and
-	stringstream(userPhoneNumber.substr(userPhoneNumber.find(')')+1 , userPhoneNumber.find('-'))) >> userRecord.phone.prefix;       //applied it to extracting the information put into the local variable for phone #
-	stringstream(userPhoneNumber.substr(userPhoneNumber.find('-')+1))>> userRecord.phone.number;                                    //and placing it into the struct data type variables
-
-    cout << endl;
-    cout << "Please enter an address: " << endl;        //user prompts
-    cout << "Input in the format of Street, City, State, Zipcode ... please include all commas." << endl;
-    getline(cin, userRecord.address.street,',');        //stores user inputs into struct variables
-    getline(cin, userRecord.address.city,',');
-    getline(cin, userRecord.address.state,',');
-    getline(cin, userRecord.address.zip,'\n');
-
-    cout << endl;
-    cout << "Please enter social media information: " << endl;      //user prompts
-    cout << "Enter social media information in the format of email, facebook, twitter ... please include all commas." << endl;
-    getline(cin, userRecord.media.email,',');       //stores user inputs into struct variables
-    getline(cin, userRecord.media.facebook,',');
-    getline(cin, userRecord.media.twitter,'\n');
-
+	
+	do
+	{
+		cout << "Enter twitter username: ";
+		cin >> userRecord.media.twitter;
+	} while (!ValidMedia(userRecord.media.twitter, 'T'));
 }
 
 //---------------------------------------------------------------------------
